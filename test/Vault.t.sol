7864bc95bbbca5d530bda3a8d71213859026dd4e
najet83 <najet.benhamouda@cimf.tn>
2025-05-10 22:03:55 +0100

    assignment14

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -19,28 +19,28 @@ contract VaultTest is Test {
 
     function testDeposit() public {
         // TODO: prank user and call deposit with 1 ether
-        vm.startPrink(user);
+        vm.prank(user);
         vault.deposit{value: 1 ether}();
         assertEq(vault.balances(user), 1 ether);
-        vm.stopPrank(); // Stop impersonating the user
+       
     }
 
     function testWithdraw() public {
         // TODO: prank user, deposit 2 ether, withdraw 1 ether
-        vm.startPrink(user);
+        vm.startPrank(user);
         vault.deposit{value: 2 ether}();
         vault.withdraw(1 ether);
-        assertEq(vault.balances(user), 1 ether);
         vm.stopPrank(); // Stop impersonating the user
+        assertEq(vault.balances(user), 1 ether);
+        
         
     }
 
     function test_RevertWithdrawMoreThanBalance() public {
         // TODO: prank user, deposit 1 ether
-        vm.startPrink(user);
+        vm.startPrank(user);
         vault.deposit{value: 1 ether}();
         vm.expectRevert();
-
         // TODO: try to withdraw 2 ether
         vault.withdraw(2 ether);
         vm.stopPrank(); // Stop impersonating the user
@@ -48,11 +48,12 @@ contract VaultTest is Test {
 
     function testGetBalance() public {
         // TODO: prank user, deposit 0.5 ether, check getBalance
-        vm.startPrink(user);
+        vm.startPrank(user);
         vault.deposit{value: 0.5 ether}();
         uint256 balance = vault.getBalance();
+        vm.stopPrank(); // Stop impersonating the user
         // TODO: assert returned balance is 0.5 ether
         assertEq(balance, 0.5 ether);
-        vm.stopPrank(); // Stop impersonating the user
+        
     }
 }
